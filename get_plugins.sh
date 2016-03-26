81272bafc792bf9198f534dcf7612be89bc89e19
Fred Thomsen <fredthomsen@fredthomsen.net>
2016-03-26 19:49:46 -0400

    Add omnicppcomplete plugin and script

--- get_plugins.sh
+++ get_plugins.sh
@@ -7,3 +7,4 @@ git clone https://github.com/tpope/vim-fugitive.git
 git clone https://github.com/ctrlpvim/ctrlp.vim.git
 git clone https://github.com/majutsushi/tagbar.git
 git clone https://github.com/scrooloose/syntastic.git
+curl -o omnicppcomplete-0.41.zip http://www.vim.org/scripts/download_script.php?src_id=7722; unzip omnicppcomplete-0.41.zip ~/.vim
