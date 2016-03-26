31c95e242164b781b4e5d0e6cc0e2acb877524f8
Fred Thomsen <fredthomsen@fredthomsen.net>
2016-03-26 19:19:19 -0400

    Add script to grab necessary plugins

---
+++ get_plugins.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd ~/.vim/bundle
+git clone https://github.com/scrooloose/nerdtree.git
+git clone https://github.com/ervandew/supertab.git
+git clone https://github.com/tpope/vim-fugitive.git
+git clone https://github.com/ctrlpvim/ctrlp.vim.git
+git clone https://github.com/majutsushi/tagbar.git
+git clone https://github.com/scrooloose/syntastic.git
